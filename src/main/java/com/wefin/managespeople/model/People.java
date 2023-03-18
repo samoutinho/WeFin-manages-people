aff948b81d77c84ff87412be215ac157ce9857bd
samoutinho <samuel.moutinho@keyks.com>
2023-03-18 18:23:52 -0300

    creating People model and repository

---
+++ src/main/java/com/wefin/managespeople/model/People.java
@@ -0,0 +1,26 @@
+package com.wefin.managespeople.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class People {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 14, nullable = false)
+    private String identifier;
+
+    @Column(length = 4, nullable = false)
+    private String identifierType;
+
+}
