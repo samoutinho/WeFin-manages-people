379307fcc3c2b69dbfe53c703863cb72cbfe3a6b
samoutinho <samuel.moutinho@keyks.com>
2023-03-19 17:49:34 -0300

    Removing  Unnecessary @Repository notation in PeopleRepository

--- src/main/java/com/wefin/managespeople/repository/PeopleRepository.java
+++ src/main/java/com/wefin/managespeople/repository/PeopleRepository.java
@@ -1,11 +1,9 @@
 package com.wefin.managespeople.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.wefin.managespeople.model.People;
 
-@Repository
 public interface PeopleRepository extends JpaRepository<People, Long> {
 
 }
