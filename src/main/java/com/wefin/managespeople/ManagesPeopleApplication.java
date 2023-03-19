f659e9fbfd4f14ee5b3dfd6a5ae249222498c98f
samoutinho <samuel.moutinho@keyks.com>
2023-03-18 22:21:55 -0300

    adding modelmapper in project dependencies

--- src/main/java/com/wefin/managespeople/ManagesPeopleApplication.java
+++ src/main/java/com/wefin/managespeople/ManagesPeopleApplication.java
@@ -1,10 +1,17 @@
 package com.wefin.managespeople;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ManagesPeopleApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagesPeopleApplication.class, args);
