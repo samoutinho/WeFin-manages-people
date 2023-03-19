7270799643b3a531bb3fb19f9003f42124887e94
samoutinho <samuel.moutinho@keyks.com>
2023-03-19 17:15:35 -0300

    adding commandLineRunner to insert row person in database

--- src/main/java/com/wefin/managespeople/ManagesPeopleApplication.java
+++ src/main/java/com/wefin/managespeople/ManagesPeopleApplication.java
@@ -1,9 +1,13 @@
 package com.wefin.managespeople;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.wefin.managespeople.model.People;
+import com.wefin.managespeople.repository.PeopleRepository;
 
 @SpringBootApplication
 public class ManagesPeopleApplication {
@@ -15,6 +19,22 @@ public class ManagesPeopleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagesPeopleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(PeopleRepository peopleRepository){
+		return args -> {
+			peopleRepository.deleteAll();
+
+			People p = new People();
+
+			p.setName("Antonio");
+			p.setIdentifier("02476586955");
+			p.setIdentifierType("CPF");
+			
+			peopleRepository.save(p);
+
+		};
 	}
 
 }
